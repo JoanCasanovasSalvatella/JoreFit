6875b1eb04007cfb23d0f6b54288501601b51728
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-02-13 17:28:40 +0100

    registro y footer 2
    comeme la vrg

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Footer2Component } from "../footer2/footer2.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, Footer2Component],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
