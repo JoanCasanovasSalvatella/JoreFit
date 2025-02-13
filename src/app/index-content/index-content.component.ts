380a24b3bdbc36788b2567b8747084eabaffcac8
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-02-13 15:56:24 +0100

    pinga

--- src/app/index-content/index-content.component.ts
+++ src/app/index-content/index-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-index-content',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './index-content.component.html',
   styleUrl: './index-content.component.css'
 })
