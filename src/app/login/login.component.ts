508e3c91e99b786ef6554060856d0d0b84cac22a
aroqiler <156545745+aroqiler@users.noreply.github.com>
2025-02-13 17:36:18 +0100

    footer2 hecho

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Footer2Component } from "../footer2/footer2.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, Footer2Component],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
