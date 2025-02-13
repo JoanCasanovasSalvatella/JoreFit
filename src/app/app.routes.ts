e56c74ecf419ee3038cabac5cb2c4ca575f90d98
aroqiler <156545745+aroqiler@users.noreply.github.com>
2025-02-13 15:55:15 +0100

    :)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { IndexContentComponent } from './index-content/index-content.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {
     path: '',
-    title: 'JoreFit',
+    title: 'JoreFit - INICIO',
     component: IndexContentComponent // Página principal
   },
-
+  {
+    path: 'login',
+    title: 'JoreFit - INICIO DE SESION',
+    component: LoginComponent // Página principal
+  },
+  {
+    path: 'register',
+    title: 'JoreFit - REGISTRO',
+    component: RegisterComponent // Página principal
+  },
 ];
