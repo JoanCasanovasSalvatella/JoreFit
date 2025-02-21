5e583faf8f629ed65240bdab9c83da0d149932fa
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-02-21 16:36:29 +0100

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,27 @@ import { Routes } from '@angular/router';
 import { IndexContentComponent } from './index-content/index-content.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { ListPlansComponent } from './list-plans/list-plans.component';
+import { ProfilComponent } from './profil/profil.component';
 
 export const routes: Routes = [
   {
     path: '',
     title: 'JoreFit - INICIO',
-    component: IndexContentComponent
+    component: IndexContentComponent // Página principal
   },
   {
     path: 'login',
     title: 'JoreFit - INICIO DE SESION',
-    component: LoginComponent
+    component: LoginComponent // Página principal
   },
   {
     path: 'register',
     title: 'JoreFit - REGISTRO',
-    component: RegisterComponent
+    component: RegisterComponent // Página principal
   },
   {
-    path: 'listPlans',
-    title: 'JoreFit - LISTA DE PLANES',
-    component: ListPlansComponent
-  }
+    path: 'profile',
+    title: 'JoreFit - PERFIL',
+    component: ProfilComponent // Perfil del usuario
+  },
 ];
