acdd2846ce6abb558f2e3b366d76bf84870cfbb3
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-02-21 16:31:12 +0100

    perfil de usuario

---
+++ src/app/profil/profil.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Footer2Component } from "../footer2/footer2.component";
+
+@Component({
+  selector: 'app-profil',
+  standalone: true,
+  imports: [Footer2Component],
+  templateUrl: './profil.component.html',
+  styleUrl: './profil.component.css'
+})
+export class ProfilComponent {
+
+}
