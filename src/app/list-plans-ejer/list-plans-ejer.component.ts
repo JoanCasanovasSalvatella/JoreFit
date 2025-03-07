d9a3f690a72bf2495728acc80c35dfdc0ee4235c
aroqiler <156545745+aroqiler@users.noreply.github.com>
2025-03-07 20:10:34 +0100

    LISTA DE PLANES DE EJERCICIOS

--- src/app/list-plans-ejer/list-plans-ejer.component.ts
+++ src/app/list-plans-ejer/list-plans-ejer.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { planes } from '../interface/planes.interface';
+import { ActivatedRoute } from '@angular/router';
+import { PlanesService } from '../services/planes.service';
+import { EjerciciosService } from '../services/ejercicios.service';
+import { ejerAsig } from '../interface/ejerAsig.interface';
+import { ejercicios } from '../interface/ejercicios.interface';
 
 @Component({
   selector: 'app-list-plans-ejer',
@@ -8,5 +14,15 @@ import { Component } from '@angular/core';
   styleUrl: './list-plans-ejer.component.css'
 })
 export class ListPlansEjerComponent {
+  selectedPlan?: planes;
+  selectedEjercicios : ejercicios [] = [];
+  selectedNivel: string = 'Leve';
 
+  constructor(private route: ActivatedRoute,
+    private planesService: PlanesService,
+    private ejerciciosService: EjerciciosService) {
+      const planId = route.snapshot.params['planId'];
+      this.selectedPlan = this.planesService.getPlan(planId);
+      this.selectedEjercicios = this.ejerciciosService.getEjerNivel(this.selectedNivel, this.selectedPlan?.nombre);
+    }
 }
