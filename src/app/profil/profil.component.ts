0963d5f841a8a7d1ed51ef700c82c1fe4b33d329
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-03-11 17:11:07 +0100

    SERVICIOS DE USUARIO

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Footer2Component } from "../footer2/footer2.component";
+import { UsuariosService } from '../services/usuarios.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [Footer2Component],
+  imports: [Footer2Component, RouterLink],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.css'
 })
 export class ProfilComponent {
-
+  constructor(public usuarios: UsuariosService){}
 }
