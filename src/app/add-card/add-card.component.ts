4fa9555b4821c07c2ad2e493d6f581b35dcf01cd
Joancs DEV <126966348+JoanCasanovasSalvatella@users.noreply.github.com>
2025-04-19 21:04:28 +0200

    FRONT_FINAL_AGREGAR-TARJETA

---
+++ src/app/add-card/add-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-add-card',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './add-card.component.html',
+  styleUrl: './add-card.component.css'
+})
+export class AddCardComponent {
+
+}
